a08d6d2aa73383c2077814317085beb759045410
Guillaume Espanel <yliugaome@gmail.com>
2012-12-11 07:14:30 +0100

    Admin the domains

--- webapp/librelist/admin.py
+++ webapp/librelist/admin.py
@@ -2,6 +2,6 @@ from webapp.librelist.models import *
 from django.contrib import admin
 
 
-for m in [Confirmation, UserState, MailingList, Subscription]:
+for m in [Confirmation, UserState, MailingList, Subscription, Domain]:
     admin.site.register(m)
 
